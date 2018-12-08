7351b482d205502dd241f8d1f4be4445d62f4f5e
aaronrbg <aaronrbg@aaronrbg.com>
2018-12-07 19:02:39 -0800

    error handling on signup form

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,5 +2,10 @@ class User < ActiveRecord::Base
 
     has_many :reviews
     has_secure_password
-    
+
+    validates :email, presence: true, uniqueness: true
+    validates :first_name, presence: true
+    validates :last_name, presence: true
+    validates :password, presence: true
+
 end
